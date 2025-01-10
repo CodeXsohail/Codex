bd733901720ba013ca0f3ca437bb06d5da03719f
karan <mrkarankr87@gmail.com>
2025-01-10 09:46:30 +0530

    checking spellings

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navlist.classList.toggle('show');
 }
+
+const inputs = document.querySelectorAll('input');
+inputs.forEach(input => {
+  input.addEventListener('focus', () => {
+    setTimeout(() => {
+      input.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }, 300); 
+  });
+});
