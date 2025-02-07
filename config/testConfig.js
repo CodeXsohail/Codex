8defaf9ac7af0ad143bfda7d492be86081b3c653
codeXsohail <sa151598680@gmail.com>
2025-02-07 09:45:25 +0530

    schedule test

--- config/testConfig.js
+++ config/testConfig.js
@@ -1,6 +1,6 @@
 const testSchedule = {
-    startTime: "2025-03-06T17:00:00", // Test start time in ISO format
-    endTime: "2025-03-06T18:00:00",   // Test end time in ISO format
+    startTime: "2025-02-06T10:00:00", // Test start time in ISO format
+    endTime: "2025-02-06T11:00:00",   // Test end time in ISO format
     duration: 60 * 60 * 1000,         // Test duration in milliseconds (60 minutes)
 };
 
