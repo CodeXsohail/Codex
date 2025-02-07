009097d58b1721c3050b0c7ef4eec2cf1f32c278
codeXsohail <sa151598680@gmail.com>
2025-02-07 09:53:36 +0530

    time zone issue fixed

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 # Code
 Me and my friends are going to organize a hackathon in our academy. For this where all information or all the things regarding this will display in the web page. The name of this event is Codex by Silicon Academy. Happy 
 
-[Go to Website](https://thekarancode0.github.io/Code/)
+[Go to Website](https://codex-bl83.onrender.com/)
 ![logo](codexLogo.png)
 ![logo](codex.png)
--- config/testConfig.js
+++ config/testConfig.js
@@ -1,7 +1,7 @@
 const testSchedule = {
-    startTime: "2025-02-07T10:00:00", // Test start time in ISO format
-    endTime: "2025-02-07T11:00:00",   // Test end time in ISO format
-    duration: 60 * 60 * 1000,         // Test duration in milliseconds (60 minutes)
+    startTime: "2025-02-07T10:30:00+05:30", // Test start time with IST timezone (+05:30)
+    endTime: "2025-02-07T11:30:00+05:30",   // Test end time with IST timezone (+05:30)
+    duration: 60 * 60 * 1000,               // Test duration in milliseconds (60 minutes)
 };
 
 module.exports = testSchedule; 
